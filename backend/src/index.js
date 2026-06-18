c765f783c8e2aa022c4aa93d9d12e7e993ab829b
Aman Sahu <amansahuat799959@gmail.com>
2026-06-18 20:18:32 +0530

    test push

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,12 @@ import express from "express";
 import connectDB from "./db/index.js";
 import app from "./app.js";
 
-connectDB()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(` Server is running at port : ${process.env.PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("PostgresSQL db connection failed !!! ", err);
-})
+// Start Express server immediately
+app.listen(process.env.PORT || 8000, () => {
+    console.log(` Server is running at port : ${process.env.PORT || 8000}`);
+});
+
+// Run DB connection check in the background
+connectDB().catch((err) => {
+    console.error("PostgresSQL database initialization failed: ", err);
+});
